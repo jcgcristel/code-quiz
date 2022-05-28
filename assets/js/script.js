073315840309cad375483c4a1141afb443a26811
jcg <jcg.cristel@gmail.com>
2022-05-28 14:04:18 -0400

    initialize repo

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// variables variables //
+
+// question array to hold question and multiple choice
+
+
+
+// display quiz question //
+
+
+
+// counter tick //
+
+
+
+// game start: cycle through quiz //
