de72ba563207ce4df2052c40bd2a2d2a88119dfd
jcg <jcg.cristel@gmail.com>
2022-05-28 14:39:59 -0400

    skeleton to display quiz questions from question object holding the question and its respective multiple choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,44 @@
-// variables variables //
+// variables //
 
-// question array to hold question and multiple choice
-
-
+// question object to hold question and multiple choice
+var questions = [
+    {
+        question: "Question 1",
+        choices: [
+            {choice: "A", answer: true},
+            {choice: "B", answer: false},
+            {choice: "C", answer: false},
+            {choice: "D", answer: false}]
+    },
+    {
+        question: "Question 2",
+        choices: [
+            {choice: "A", answer: false},
+            {choice: "B", answer: true},
+            {choice: "C", answer: false},
+            {choice: "D", answer: false}]
+        },
+]
 
 // display quiz question //
+var showQuestion = function() {
+    for (var i = 0; i < questions.length; i++) {
+        // print question
+        console.log(questions[i].question);
+    
+        // print question choices
+        showQuestionChoices(questions[i]);
+    }    
+}
 
+// display quiz question choices
+var showQuestionChoices = function(question) {
+    var questionChoice = question.choices;
+
+    for (var i = 0; i < questionChoice.length; i++) {
+        console.log(`${questionChoice[i].choice} ${questionChoice[i].answer}`);
+    }
+}
 
 
 // counter tick //
@@ -13,3 +46,4 @@
 
 
 // game start: cycle through quiz //
+showQuestion();
